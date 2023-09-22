72a328544e49e1bf4d3d67d45fa47a4c56293f76
Andrej Kuzmanoski <andrejkuzmanoski2@gmail.com>
2023-09-22 02:02:25 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Create from './Create';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
 import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
 
 export const ThemeContext = createContext(null)
 
 function App() {
-  const [theme, setTheme] = useState("dark")
+  const [theme, setTheme] = useState("light")
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"))
@@ -18,6 +19,10 @@ function App() {
     <ThemeContext.Provider value={{theme, toggleTheme}}>
       <Router>
         <div className="App" id={theme}>
+          <div className='switch'>
+            <label>{ theme === "light" ? "Light Mode" : "Dark Mode" }</label>
+            <ReactSwitch onChange={toggleTheme} checked = {theme === "dark"}/>
+          </div>
           <Navbar />
           <div className="content">
             <Switch>
