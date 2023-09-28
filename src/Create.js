fac0f9bf33af74f5deb34a275bbce3e1c3eebcdb
Andrej Kuzmanoski <andrejkuzmanoski2@gmail.com>
2023-09-28 21:38:39 +0200

    Update

--- src/Create.js
+++ src/Create.js
@@ -14,7 +14,7 @@ const Create = () => {
 
         setIsPending(true);
 
-        fetch('http://localhost:8000/blogs', {
+        fetch('../data/db.json', {
             method: 'POST',
             headers: { "Content-Type": "application/json"},
             body: JSON.stringify(blog)
